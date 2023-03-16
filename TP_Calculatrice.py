6aaf138325b8dd4afbee4e106cd58efa2aecdf5a
ZerKaouthar <kou.zerhouni@gmail.com>
2023-03-16 14:35:17 +0100

    commit

--- TP_Calculatrice.py
+++ TP_Calculatrice.py
@@ -1,11 +1,11 @@
 #Somme de deux nombre 
-def DeuxNombre(f1,f2):
-    ''' La fonction suivante calcule la somme de deux nombre '''
-    return f1+f2
+def DeuxNombre(num1_,num2_):
+     """La fonction suivante calcule la somme de deux nombre""" 
+    return num1_+num2_
 
 
 #Produit de deux nombre
-def DeuxNombre(f1,f2):
-    ''' La fonction suivante calcule le produit de deux nombre '''
-    return f1*f2
+def DeuxNombre(num1_,num2_):
+    """La fonction suivante calcule le produit de deux nombre"""
+    return num1_*num2_
     
--- test_1.py
+++ test_1.py
@@ -1,10 +1,12 @@
+"""test func 1"""
 def test_DeuxNombre():
-    # Test addition
+     """Test addition"""
     assert DeuxNombre(2, 3) == 5
     assert DeuxNombre(-2, 3) == 1
     assert DeuxNombre(0, 0) == 0
 
-    # Test multiplication
+def test_MultipNombre():
+    """Test multiplication"""
     assert DeuxNombre(2, 3) == 6
     assert DeuxNombre(-2, 3) == -6
     assert DeuxNombre(0, 0) == 0
