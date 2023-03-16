00bec187ffeb25ad56ad2f6897f2714d57f5524f
ZerKaouthar <kou.zerhouni@gmail.com>
2023-03-16 14:27:25 +0100

    commit

--- TP_Calculatrice.py
+++ TP_Calculatrice.py
@@ -1,8 +1,11 @@
 #Somme de deux nombre 
-def DeuxNombre(a,b):
-    return a+b
+def DeuxNombre(f1,f2):
+    ''' La fonction suivante calcule la somme de deux nombre '''
+    return f1+f2
 
 
 #Produit de deux nombre
-def DeuxNombre(a,b):
-    return a*b
+def DeuxNombre(f1,f2):
+    ''' La fonction suivante calcule le produit de deux nombre '''
+    return f1*f2
+    
